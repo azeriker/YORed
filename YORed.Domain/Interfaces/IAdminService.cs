46afb60085e0c73d8a7c94e919d46dd6e91b34c7
Максим <azeriker@gmail.com>
2019-07-03 14:55:05 +0300

    Added controller Added Views

---
+++ YORed.Domain/Interfaces/IAdminService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YORed.Domain.Interfaces
+{
+    public interface IAdminService
+    {
+        
+    }
+
+}
---
+++ YORed/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using YORed.Domain.Interfaces;
+
+namespace YORed.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IConfiguration _configuration;
+
+        public AdminController(IConfiguration configuration, IUserService userService)
+        {
+            _configuration = configuration;
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string login, string password)
+        {
+            var adminLogin = _configuration.GetSection("adminLogin").Value;
+            var adminPassword = _configuration.GetSection("adminPassword").Value;
+            if (login == adminLogin && password == adminPassword)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, login),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, "Admin")
+                };
+                ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+            }
+            return RedirectToAction("Index", "Admin");
+
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
