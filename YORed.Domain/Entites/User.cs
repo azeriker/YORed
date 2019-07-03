0fcac4f1bfbf83d51db6ca47d073b2f7cf9f48cf
Максим <azeriker@gmail.com>
2019-07-03 11:51:40 +0300

    Added user entity

---
+++ YORed.Domain/Entites/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace YORed.Domain.Entites
+{
+    public enum UserRole
+    {
+        User,
+        Moderator,
+        Admin
+    }
+    public class User
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public Login Login { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+    }
+
+    public class Login
+    {
+        public Login(string phone)
+        {
+            Phone = phone;
+        }
+        public string Phone { get; }
+    }
+}
